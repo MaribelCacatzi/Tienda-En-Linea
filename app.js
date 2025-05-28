e18f1dcc7e336d07f0687974f679fae4a763379c
Joseline Cacatzi <joselinecacatzi42@gmail.com>
2025-05-28 12:27:36 -0600

    Nuevos cambios realizados

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 let contenedor = document.querySelector('#RowProductos')
 let canva = document.querySelector('#ContenedorCanva')
+let spiner = document.querySelector('#spiner')
 
 let url = `https://api.escuelajs.co/api/v1/products?offset=0&limit=10`
 
 let productos;
+let nuevo = ``
 
-fetch(url).then(response =>{
-    return response.json()
-}).then(datos =>{
-    console.log(datos)
+const ejecutar = () =>{
+    setTimeout(() =>{
+        fetch(url).then(response => {
+            return response.json()
+        }).then(datos => {
+            console.log(datos)
+        
+            const producto = datos.map(item => {
+                return productos = {
+                    name: item.title,
+                    img: item.images,
+                    price: item.price
+                }
+            })
+        
+            producto.forEach(element => {
+                contenedor.innerHTML += card(element)
+            });
+        
+        }).catch(error => {
+            alert('Ocurrio un error')
+            console.error('error', error)
+        }).finally(()=>{
+            spiner.className='spinner-none'
+        })
+    },1000)
+}
 
-    const producto = datos.map(item =>{
-        return productos = {
-            name: item.category.name,
-            img: item.category.image,
-            precio: item.price
-        } 
-    })
-    
-    producto.forEach(element => {
-        contenedor.innerHTML+= card(element)
-    });
+ejecutar()
 
-}).catch(error =>{
-    console.error('error', error)
-})
-
-const card = (producto) =>{
+const card = (producto) => {
     return `
     <div class="col-12 col-md-4 col-lg-3 mb-3">
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 18rem; height:;">
             <img src="${producto.img}" class="card-img-top" alt="${producto.name}">
             <div class="card-body">
                 <h5 class="card-title">${producto.name}</h5>
-                <p class="card-text">Precio: ${producto.precio}</p>
-                <button class="btn AñadirAlCarrito" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                <p class="card-text">Precio: ${producto.price}</p>
+                <button class="btn AñadirAlCarrito" onclick="datosCanvaEnviar('Producto:${producto.name}, Precio: ${producto.price}, Imagen: ${producto.img}')" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
                 aria-controls="offcanvasRight">Añadir al carrito</button>
             </div>
         </div>
@@ -42,19 +53,15 @@ const card = (producto) =>{
     `
 }
 
-const datosCanva = (producto) =>{
-    let pro= `
-    <div class="col-12 col-md-4 col-lg-3 mb-3">
-        <div class="card" style="width: 10rem;">
-            <img src="${producto.img}" class="card-img-top" alt="${producto.name}">
-            <div class="card-body">
-                <h5 class="card-title">${producto.name}</h5>
-                <p class="card-text">Precio: ${producto.precio}</p>
-                <button class="btn AñadirAlCarrito" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-                aria-controls="offcanvasRight">Añadir al carrito</button>
-            </div>
+const datosCanvaEnviar = (producto) => {
+    let pro = `
+        <div class="card mb-3" style="width: 18rem;">
+            <p class="card-title">${producto}</p>
         </div>
-    </div>
     `
-    localStorage.setItem('NuevoProducto', JSON.stringify(pro))
+    localStorage.setItem('NuevoProducto', pro)
+    canva.innerHTML += localStorage.getItem('NuevoProducto' || '')
 }
+
+canva.innerHTML += localStorage.getItem('NuevoProducto' || '')
+
